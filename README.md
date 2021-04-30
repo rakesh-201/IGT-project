fa5f724dd9dfb51742fe0735a61f9bdb2a14c13e
Rakesh Rajpurohit <rakesh.rajpurohit.rr.rr.201@gmail.com>
2021-04-30 13:40:19 +0000

    project completed and README half way done.

---
+++ README.md
@@ -0,0 +1,38 @@
+# MAPIFY - map it!
+
+## Description
+
+
+
+## Installation
+
+Open up your terminal and run the following command:
+
+`npm i mapify`
+
+For `yarn` users
+
+`yarn add mapify`
+
+## Getting Started
+
+To use the package you will need to do a couple of things.
+
+First, add the following scripts inside of your `HTML` head tag:
+
+```
+<script src="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-directions/v4.1.0/mapbox-gl-directions.js"></script>
+<script src="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v4.7.0/mapbox-gl-geocoder.min.js"></script>
+```
+
+After this is done, go to mapbox.com and get a free account.
+
+After your account is created, select the type of your application ( _Web/Android/iOS_ ). After this click on CDN and grab your access token.
+
+And that's it...
+
+See the example given below for understanding the usage.
+
+## Options
+
+## Example
--- index.js
+++ index.js
@@ -1,32 +1,59 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoicmJyYWpwdXJvaGl0IiwiYSI6ImNrbW90dnhiMDBqZDkycXJ2ZXlzcDRjbXoifQ.Xi45YofolrAQXbxxRcGTIw';
+const mapify = (options) => {
 
-const successLoc = (position) => {
-    renderMap([position.coords.longitude, position.coords.latitude]);
-}
+    var href = "https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-directions/v4.1.0/mapbox-gl-directions.css";
 
-const errorLoc = () => {
-    renderMap([72.8777, 19.0760]);
-}
+    if (options.searchOnly) {
+        href = "https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v4.7.0/mapbox-gl-geocoder.css"
+    }
 
-navigator.geolocation.getCurrentPosition(successLoc, errorLoc, {
-    enableHighAccuracy: true
-})
+    var a = document.createElement('link');
+    a.rel = "stylesheet"
+    a.href = href
+    a.type = "text/css"
+    document.body.appendChild(a);
 
+    mapboxgl.accessToken = options.accessToken;
 
-const renderMap = (location) => {
-    var map = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/mapbox/streets-v11',
-        center: location,
-        zoom: 15
-    });
+    const successLoc = (position) => {
+        renderMap([position.coords.longitude, position.coords.latitude]);
+    }
 
-    const nav = new mapboxgl.NavigationControl()
-    map.addControl(nav)
+    const errorLoc = () => {
+        renderMap([72.8777, 19.0760]);
+    }
 
-    const directions = new MapboxDirections({
-        accessToken: mapboxgl.accessToken
+    navigator.geolocation.getCurrentPosition(successLoc, errorLoc, {
+        enableHighAccuracy: true
     })
 
-    map.addControl(directions, "top-left")
+    const renderMap = (location) => {
+        var map = new mapboxgl.Map({
+            container: 'map',
+            style: 'mapbox://styles/mapbox/streets-v11',
+            center: location,
+            zoom: 15
+        });
+
+        if (options.searchOnly) {
+            map.addControl(
+                new MapboxGeocoder({
+                    accessToken: mapboxgl.accessToken,
+                    mapboxgl: mapboxgl
+                })
+                , "top-left"
+            );
+        }
+        else {
+            const nav = new mapboxgl.NavigationControl()
+            map.addControl(nav)
+
+
+            const directions = new MapboxDirections({
+                accessToken: mapboxgl.accessToken
+            })
+            map.addControl(directions, "top-left")
+        }
+    }
 }
+
+module.exports.mapify = mapify;
